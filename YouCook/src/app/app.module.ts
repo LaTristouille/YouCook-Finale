951dde2d37559d6ee880de65f65cabf4fcc6b748
John Doe <tristan.varciat@gmail.com>
2021-05-05 16:37:16 +0200

    firebase implementation, list start

--- YouCook/src/app/app.module.ts
+++ YouCook/src/app/app.module.ts
@@ -7,11 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyhIJl4xttQMyfYwytf0EW4xt3q2N9isM",
+  authDomain: "youcook-c3088.firebaseapp.com",
+  databaseURL: "https://youcook-c3088-default-rtdb.firebaseio.com",
+  projectId: "youcook-c3088",
+  storageBucket: "youcook-c3088.appspot.com",
+  messagingSenderId: "678417005657",
+  appId: "1:678417005657:web:ff1c26e5b4294437770548"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
+
+
+
 export class AppModule {}
--- YouCook/src/app/tab1/tab1.page.ts
+++ YouCook/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  myRecette: string;
+  addRecette: boolean;
+  recettes = [];
 
-  constructor() {}
+  constructor(public afDB: AngularFireDatabase) {
+    this.getRecettes();
+
+  }
+  addRecetteToFirebase(){
+    console.log('myRecette:' + this.myRecette)
+    this.afDB.list('Recettes/').push({
+      ingredient: 'je suis un ingredient',
+      detailRecette: this.myRecette,
+      nutriscore: 'A',
+      nom: 'Patatas bravas',
+    });
+    this.showForm();
+  }
+
+  showForm() {
+this.addRecette = !this.addRecette;
+this.myRecette = '';
+  }
+
+  getRecettes() {
+    this.afDB.list('Recettes/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      actions.forEach(action =>{
+        console.log(action.payload.exportVal().detailRecette);
+        this.recettes.push({
+          key: action.key,
+          detailRecette: action.payload.exportVal().detailRecette, 
+          ingredient: action.payload.exportVal().ingredient, 
+          nom: action.payload.exportVal().nom, 
+          nutriscore: action.payload.exportVal().nutriscore, 
+        })
+      });
+    })
+  }
 
 }
